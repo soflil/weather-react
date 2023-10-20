b591b79e101941b1eaf4f4bc61c8ddf838847922
soflil <134602526+soflil@users.noreply.github.com>
2023-10-20 14:23:55 +0200

    Added a link to the source-code and to my linkedIn

--- src/App.js
+++ src/App.js
@@ -5,6 +5,23 @@ function App() {
   return (
     <div className="App">
       <Weather />
+      <p>
+        <a
+          href="https://github.com/soflil/weather-react"
+          target="_blank"
+          rel="noreferrer"
+        >
+          Open-source code
+        </a>
+        , by{" "}
+        <a
+          href="https://www.linkedin.com/in/sophie-schou-166b85278/"
+          target="_blank"
+          rel="noreferrer"
+        >
+          Sophie Schou
+        </a>
+      </p>
     </div>
   );
 }
